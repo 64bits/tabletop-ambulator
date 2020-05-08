81f44483f46703a65811f337efd0e8dda3b3b691
Rohit Garg <rohit@unity3d.com>
2020-05-07 19:38:36 -0700

    Reject files over 5 MB

--- server/image-processor.js
+++ server/image-processor.js
@@ -4,6 +4,8 @@ const sharp = require('sharp');
 
 cachedRequest.setCacheDirectory('/tmp/cache');
 
+const IMAGE_MAX_MB = 5;
+
 const imageProcessor = (req, res) => {
   const { url: imageUrl, offset, width: sheetWidth, height: sheetHeight, thumb } = req.query;
   if(!imageUrl || !offset || !sheetWidth || !sheetHeight) {
@@ -12,46 +14,55 @@ const imageProcessor = (req, res) => {
   }
   const thumbFactor = thumb ? 5 : 1;
   const remoteImageUrl = unescape(imageUrl);
-  // const protocol = remoteImageUrl.startsWith('https') ? https : http;
-  cachedRequest.get({
+  request({
     url: remoteImageUrl,
-    encoding: null,
-    ttl: 30 * 60 * 1000 // 30 minutes
-  }, function (err, response, body) {
-    const image = sharp(body);
-    image
-      .metadata()
-      .then(({ width, height, format }) => {
-        // Calculate card size based on the sheet info and image dims
-        const cardWidth = ~~(width / sheetWidth);
-        const cardHeight = ~~(height / sheetHeight);
-        const left = (offset % sheetWidth) * cardWidth;
-        const top = Math.floor(offset / sheetWidth) * cardHeight;
-        image
-          .resize({
-            width: ~~(width / thumbFactor),
-            height: ~~(height / thumbFactor),
-          })
-          .extract({
-            left: ~~(left / thumbFactor),
-            top: ~~(top / thumbFactor),
-            width: ~~(cardWidth / thumbFactor),
-            height: ~~(cardHeight / thumbFactor)
-          })
-          .toBuffer()
-          .then(data => {
-            res.type(format);
-            res.end(data);
-          })
-          .catch(err => {
-            console.log(err);
-            res.redirect('https://i.imgur.com/WwuvEPd.jpg');
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        res.redirect('https://i.imgur.com/WwuvEPd.jpg');
-      });
+    method: 'HEAD'
+  }, function(err, response, body) {
+    if (response.headers['content-length'] / (1024 * 1024) > IMAGE_MAX_MB) {
+      res.redirect('https://i.imgur.com/kPXLU8G.jpg');
+      return;
+    }
+    // Proceed if image is small enough
+    cachedRequest.get({
+      url: remoteImageUrl,
+      encoding: null,
+      ttl: 30 * 60 * 1000 // 30 minutes
+    }, function (err, response, body) {
+      const image = sharp(body);
+      image
+        .metadata()
+        .then(({ width, height, format }) => {
+          // Calculate card size based on the sheet info and image dims
+          const cardWidth = ~~(width / sheetWidth);
+          const cardHeight = ~~(height / sheetHeight);
+          const left = (offset % sheetWidth) * cardWidth;
+          const top = Math.floor(offset / sheetWidth) * cardHeight;
+          image
+            .resize({
+              width: ~~(width / thumbFactor),
+              height: ~~(height / thumbFactor),
+            })
+            .extract({
+              left: ~~(left / thumbFactor),
+              top: ~~(top / thumbFactor),
+              width: ~~(cardWidth / thumbFactor),
+              height: ~~(cardHeight / thumbFactor)
+            })
+            .toBuffer()
+            .then(data => {
+              res.type(format);
+              res.end(data);
+            })
+            .catch(err => {
+              console.log(err);
+              res.redirect('https://i.imgur.com/WwuvEPd.jpg');
+            });
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('https://i.imgur.com/WwuvEPd.jpg');
+        });
+    });
   });
 };
 
